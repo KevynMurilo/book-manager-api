0536bd2cbb2a44b698c56c4900754b71a2c6edf9
Kevyn Murilo <132490286+KevynMurilo@users.noreply.github.com>
2023-10-16 08:30:54 -0300

    Update livraria.js

--- routes/livraria.js
+++ routes/livraria.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer");
-const PictureController = require("../controllers/livrariaController");
+const BookController = require("../controllers/livrariaController");
 
-// Rota para criar um novo Livraria item
-router.post("/", upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), PictureController.create);
+// Rota para criar um novo livro
+router.post("/", upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), BookController.create);
 
-// Rota para obter todas as imagens
-router.get("/", PictureController.findAll);
+// Rota para obter todos os livros
+router.get("/", BookController.findAll);
 
-// Rota para obter o PDF de um Livraria item
-router.get("/:id/pdf", PictureController.getPDF);
+// Rota para obter o PDF de um livro
+router.get("/:id/pdf", BookController.getPDF);
 
-// Adicione a rota para obter a imagem
-router.get("/:id/image", PictureController.getImage);
+// Adicione a rota para obter a imagem de um livro
+router.get("/:id/image", BookController.getImage);
 
-// Rota para remover um Livraria item
-router.delete("/:id", PictureController.remove);
+// Rota para remover um livro
+router.delete("/:id", BookController.remove);
 
 module.exports = router;
