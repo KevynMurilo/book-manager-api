10aa7c2e7b2d29f70a8bd678a78e29fc5a7a278d
Kevyn Murilo <132490286+KevynMurilo@users.noreply.github.com>
2023-10-18 18:20:31 -0300

    Update livraria.js

--- routes/livraria.js
+++ routes/livraria.js
@@ -3,19 +3,16 @@ const router = express.Router();
 const upload = require("../config/multer");
 const BookController = require("../controllers/livrariaController");
 
-// Rota para criar um novo livro
 router.post("/", upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), BookController.create);
 
-// Rota para obter todos os livros
+router.put("/:id", upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), BookController.update);
+
 router.get("/", BookController.findAll);
 
-// Rota para obter o PDF de um livro
 router.get("/:id/pdf", BookController.getPDF);
 
-// Adicione a rota para obter a imagem de um livro
 router.get("/:id/image", BookController.getImage);
 
-// Rota para remover um livro
 router.delete("/:id", BookController.remove);
 
 module.exports = router;
